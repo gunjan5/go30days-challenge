4b182e40e3334f5dcb79c13bfeb07cf410e690d6
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-08 01:23:32 -0700

    Adding net/http server

---
+++ day1/web/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "gunjan5/go30days-challenge/tree/master/day1/gitio"
+)
+
+var (
+	longUrl  string
+	)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    //fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    longUrl = r.URL.Path[1:]
+    shortUrl, err := gitio.Shorten(longUrl)
+    if err != nil {
+    	fmt.Fprintf("Oh man! You broke it! %s", err)
+    }
+    fmt.Fprintf(shortUrl)
+
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
