15567fabf115413a9ed150822f18e70d4da93177
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-08 03:21:17 -0700

    RespWriter

--- day1/web/server.go
+++ day1/web/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
     "github.com/gunjan5/go30days-challenge/day1/gitio"
 )
@@ -10,22 +11,44 @@ var (
 	longUrl  string
 	)
 
+type Page struct {
+    		Title string
+    		Body  []byte
+    	}
+
 func handler(w http.ResponseWriter, r *http.Request) {
     //fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-    //longUrl = r.URL.Path[1:]
-    longUrl = "https://github.com/gunjan5/go30days-challenge/tree/master/day1"
+    longUrl = r.URL.Path[1:]
+    fmt.Println(longUrl)
+    //longUrl = "https://github.com/gunjan5/go30days-challenge/tree/master/day1"
     shortUrl, err := gitio.Shorten(longUrl)
     if err != nil {
     	fmt.Fprintf(w, "Oh man! You broke it! %s", err)
     } else {
-    	fmt.Println(w, shortUrl)
+    	fmt.Fprintf(w, "%s", shortUrl)
     }
     
 
 }
 
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/view/", viewHandler)
     http.ListenAndServe(":8080", nil)
     // longUrl = "https://github.com/gunjan5/go30days-challenge/tree/master/day1"
     // shortUrl, err := gitio.Shorten(longUrl)
