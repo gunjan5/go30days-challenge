2e12842848a020e9710a7aa2f445e0206202274d
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-10 00:44:48 -0700

    Websocket server

--- day3-angularjs-golang/main.go
+++ day3-angularjs-golang/main.go
@@ -4,12 +4,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
+func wsHandler ( w http.ResponseWriter, r *http.Request) {
+
+	conn, err := upgrader.Upgrade(w,r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	for {
+		_, msg, err := conn.ReadMessage() 
+		if err != nil {
+			return
+		}
+		log.Println(string(msg))
+		if err = conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			return
+		}
+
+	}
+}
 func main() {
 	// handle all requests by serving a file of the same name
 	fileHandler := http.FileServer(http.Dir("web/"))
 	http.Handle("/", fileHandler)
+	http.HandleFunc("/ws", wsHandler)
 
 	log.Printf("Running on localhost:8080")
 	addr := fmt.Sprintf("127.0.0.1:8080")
