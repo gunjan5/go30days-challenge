19128298934b7fcbb47fdbcce1808691b52e12a8
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-11 00:22:01 -0700

    body

--- day4-REST-JSON-APIs/main.go
+++ day4-REST-JSON-APIs/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+"encoding/json"
+"fmt"
+"net/http"
+)
+
+
+func serveRest ( w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+
+	http.HandleFunc("/", servRest)
+
+	http.ListenAndServe("localhost:8080", nil)
+	
+
+}
+
+
