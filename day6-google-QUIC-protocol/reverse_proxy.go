f5251158d8fdc918c2744f917a7938e2dcf85d4f
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-13 00:37:25 -0700

    reverse proxy, client, server

--- day6-google-QUIC-protocol/reverse_proxy.go
+++ day6-google-QUIC-protocol/reverse_proxy.go
@@ -4,39 +4,47 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 
 	"github.com/devsisters/goquic"
 )
 
 var numOfServers int
 var port int
-var serveRoot string
 var logLevel int
 var cert string
 var key string
 
-func httpHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("This is an example server.\n"))
-}
-
 func init() {
 	flag.IntVar(&numOfServers, "n", 1, "Number of concurrent quic dispatchers")
 	flag.IntVar(&port, "port", 8080, "TCP/UDP port number to listen")
-	flag.StringVar(&serveRoot, "root", "/tmp", "Root of path to serve under https://127.0.0.1/files/")
 	flag.IntVar(&logLevel, "loglevel", -1, "Log level")
 	flag.StringVar(&cert, "cert", "", "Certificate file (PEM), will use encrypted QUIC and SSL when provided")
 	flag.StringVar(&key, "key", "", "Private key file (PEM), will use encrypted QUIC and SSL when provided")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s backend_url\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
 }
 
 func main() {
 	flag.Parse()
 	goquic.SetLogLevel(logLevel)
 
+	if flag.NArg() != 1 {
+		flag.Usage()
+		return
+	}
+
 	useEncryption := false
 	if len(cert) > 0 && len(key) > 0 {
 		useEncryption = true
 	}
+	proxyUrl := flag.Arg(0)
 
 	scheme := "http"
 	if useEncryption {
@@ -45,17 +53,18 @@ func main() {
 	log.Printf("About to listen on %d. Go to %s://127.0.0.1:%d/", port, scheme, port)
 	portStr := fmt.Sprintf(":%d", port)
 
-	http.HandleFunc("/", httpHandler)
-	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir(serveRoot))))
-
-	var err error
-
-	if useEncryption {
-		err = goquic.ListenAndServeSecure(portStr, cert, key, numOfServers, nil)
-	} else {
-		err = goquic.ListenAndServe(portStr, numOfServers, nil)
+	parsedUrl, err := url.Parse(proxyUrl)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	log.Printf("Starting reverse proxy for backend URL: %v", parsedUrl)
+
+	if useEncryption {
+		err = goquic.ListenAndServeSecure(portStr, cert, key, numOfServers, httputil.NewSingleHostReverseProxy(parsedUrl))
+	} else {
+		err = goquic.ListenAndServe(portStr, numOfServers, httputil.NewSingleHostReverseProxy(parsedUrl))
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
