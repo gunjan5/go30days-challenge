1ac7233d4a698fdb611751fd3866bf38fff44600
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-14 00:27:41 -0700

    syntax error

--- day7-goroutines-channels-1/goroutines.go
+++ day7-goroutines-channels-1/goroutines.go
@@ -4,6 +4,7 @@ package main
 import (
   "fmt"
   "runtime"
+  "time"
 
 )
 
@@ -12,6 +13,7 @@ func sayMyName(name string){
 	for i:=0; i<5; i++ {
 		runtime.Gosched()
 		fmt.Println("Oh na na what's my name? ", name)
+		time.Sleep(100*time.Millisecond) //doing some fake work here... please wait ZzzZzzZzzz
 	}
 	
 }
@@ -23,7 +25,7 @@ func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	fmt.Println("You've got" runtime.NumCPU() " CPU cores yo!")
+	fmt.Println("You've got", runtime.NumCPU() , " CPU cores yo!")
 
 	sayMyName("basic")
 
