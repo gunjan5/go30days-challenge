547878da59bd242a435ab4e1921e593ac6962115
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-19 00:32:10 -0700

    message bus with redis

--- day12-redis/redis.go
+++ day12-redis/redis.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+)
+
+func main() {
+
+	c, err := redis.Dial("tcp", ":6379")
+
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	c.Do("SET", "message1", "yello humans!")
+
+	world, err := redis.String(c.Do("GET", "message1"))
+	if err != nil {
+		fmt.Println("key not found yo")
+		panic(err)
+	}
+
+	fmt.Println(world)
+}
