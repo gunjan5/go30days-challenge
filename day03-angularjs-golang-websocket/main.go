f0ad59314bdddd57accddd4764cbe9e077f47456
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-23 00:01:11 -0700

    CF ready checkpoint 1

--- day03-angularjs-golang-websocket/main.go
+++ day03-angularjs-golang-websocket/main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
+	"os"
 )
 
-
-
-var connections map[*websocket.Conn] bool
+var connections map[*websocket.Conn]bool
 
 func sendAll(msg []byte) {
 	for conn := range connections {
 		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
 			delete(connections, conn)
-		      return
+			return
+		}
 	}
 }
-}
-func wsHandler ( w http.ResponseWriter, r *http.Request) {
+func wsHandler(w http.ResponseWriter, r *http.Request) {
 
-	conn, err := websocket.Upgrade(w,r, nil, 1024, 1024)
-	if _,ok := err.(websocket.HandshakeError); ok {
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
 		http.Error(w, "Not a websocket handshake", 400)
 		return
 	} else if err != nil {
@@ -32,7 +31,7 @@ func wsHandler ( w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 	connections[conn] = true
 	for {
-		_, msg, err := conn.ReadMessage() 
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			delete(connections, conn)
 			return
@@ -44,14 +43,14 @@ func wsHandler ( w http.ResponseWriter, r *http.Request) {
 }
 func main() {
 
-	connections = make(map[*websocket.Conn] bool)
+	connections = make(map[*websocket.Conn]bool)
 	// handle all requests by serving a file of the same name
 	fileHandler := http.FileServer(http.Dir("web/"))
 	http.Handle("/", fileHandler)
 	http.HandleFunc("/ws", wsHandler)
 
-	log.Printf("Running on localhost:8080")
-	addr := fmt.Sprintf("127.0.0.1:80")
+	//log.Printf("Running on localhost:8080")
+	addr := fmt.Sprintf("127.0.0.1:8080")
 	// this call blocks -- the progam runs here forever
 	err := http.ListenAndServe(addr, nil)
 	fmt.Println(err.Error())
