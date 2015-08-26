0febe6f312e0c5fcdd87c3eeff208bff312d9ff6
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-26 01:27:54 -0700

    OOP-ized fileCreate and chmod

--- day17-files/fileOps.go
+++ day17-files/fileOps.go
@@ -10,11 +10,42 @@ var (
 	err     error
 )
 
-func main() {
-	newFile, err = os.Create("meNew.txt")
+//use array [...]
+//slices
+//performance
+
+type Gfile struct {
+	Name    string
+	Size    float32
+	Mode    os.FileMode
+	Content []string
+
+	//permissions, owners, last modified
+}
+
+func (f *Gfile) createFile() {
+	newFile, err = os.Create(f.Name)
 	if err != nil {
 		panic(err)
 	}
+	defer newFile.Close()
 	log.Println(newFile)
-	newFile.Close()
+
+}
+
+func (f *Gfile) changemod(mode os.FileMode) {
+	err := os.Chmod(f.Name, mode)
+	if err != nil {
+		panic(err)
+	}
+	f.Mode = mode //should this be in a defer?
+	log.Println("changed the file mode to", f.Mode, "for file: ", f.Name)
+
+}
+
+func main() {
+	f1 := Gfile{Name: "newGfile.txt", Content: []string{"line1", "line2", "line3"}}
+	f1.createFile()
+	f1.changemod(0777)
+
 }
