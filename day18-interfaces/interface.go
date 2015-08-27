b17899aa861397d07ffcc29822e9fb294ce0ebcd
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-26 23:47:22 -0700

    checkpoint 2

--- day18-interfaces/interface.go
+++ day18-interfaces/interface.go
@@ -9,7 +9,16 @@ type User struct {
 }
 
 func (u *User) Name() string {
-	return fmt.Sprintf("%s %s", i.FName, u.Lname)
+	return fmt.Sprintf("%s %s", u.FName, u.LName)
+}
+
+type Customer struct {
+	Id       int
+	FullName string
+}
+
+func (c *Customer) Name() string {
+	return c.FullName
 }
 
 type Namer interface {
@@ -22,4 +31,7 @@ func Greet(n Namer) string {
 func main() {
 	u := &User{"Gunjan", "Patel"}
 	fmt.Println(Greet(u))
+
+	c := &Customer{12, "c3po"}
+	fmt.Println(Greet(c))
 }
