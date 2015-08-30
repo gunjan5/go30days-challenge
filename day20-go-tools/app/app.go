9b3cad01ce0b9780112263fb43e832504ba9ee7d
Gunjan Patel <patelgunjan5@gmail.com>
2015-08-30 01:31:19 -0700

    cleanup

--- day20-go-tools/app/app.go
+++ day20-go-tools/app/app.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Hello)
 	http.Handle("/", r)
-	fmt.Println("Starting up on 8080")
+	fmt.Println("Starting the server on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func Hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Hello world!")
+	fmt.Fprintln(w, "Hello from Go tools!")
 }
