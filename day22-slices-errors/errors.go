293f494f93829d6d34c0aec7c46872fcdc16b52d
Gunjan Patel <patelgunjan5@gmail.com>
2015-09-03 23:00:09 -0700

    errors

--- day22-slices-errors/errors.go
+++ day22-slices-errors/errors.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var (
+	ErrorEmptyString = errors.New("Not gonna yell an epmpty message yo!, gimme something more meaningful") //exported type
+)
+
+func yellit(msg string) error {
+
+	if msg == "" {
+		return ErrorEmptyString
+		// panic(ErrorEmptyString) //do this only if something is really bad panic and recover aint so good for normally bad things
+
+	}
+	_, err := fmt.Printf("%s\n", msg)
+
+	return err
+
+}
+
+func main() {
+
+	if err := yellit(""); err != nil {
+		fmt.Printf("yelling didnt work dude: %s\n", err)
+		os.Exit(1)
+	}
+}
