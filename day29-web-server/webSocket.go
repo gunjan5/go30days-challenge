377c044e9b4884a5cac60b019145a06fb62c40cc
Gunjan Patel <patelgunjan5@gmail.com>
2016-01-23 00:52:22 -0800

    json over websocket

--- day29-web-server/webSocket.go
+++ day29-web-server/webSocket.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"time"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -15,7 +16,7 @@ func main() {
 	})
 
 	http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
-		 conn, _ := upgrader.Upgrade(w, r, nil)
+		conn, _ := upgrader.Upgrade(w, r, nil)
 		go func(conn *websocket.Conn) {
 
 			for {
@@ -27,5 +28,38 @@ func main() {
 
 	})
 
+	http.HandleFunc("/v2/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+
+			for {
+				_, msg, _ := conn.ReadMessage()
+				fmt.Println(string(msg))
+			}
+
+		}(conn)
+
+	})
+
+		http.HandleFunc("/v3/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			ch:= time.Tick(5* time.Second)
+			for range ch{
+				conn.WriteJSON(myStruct{
+					User: "gunjan5",
+					Name: "Gunjan Patel",
+					})
+			}
+
+
+		}(conn)
+
+	})
+
 	http.ListenAndServe(":8080", nil)
+}
+type myStruct struct{
+	User string `json:"user"`
+	Name string `json:"name"`
 }
