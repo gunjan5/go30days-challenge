16d382fd58c77c97899c8857d6bcad135a38570b
Gunjan Patel <patelgunjan5@gmail.com>
2016-01-31 23:34:40 -0800

    this is how Todd does it

--- day11-mutex-events-models-5/mutex.go
+++ day11-mutex-events-models-5/mutex.go
@@ -1,9 +1,42 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"sync"
+// )
+
+// func init() {
+// 	runtime.GOMAXPROCS(runtime.NumCPU())
+// }
+
+// func main() {
+
+// 	mutex := new(sync.Mutex)
+
+// 	for i := 0; i < 10; i++ {
+// 		//fmt.Println(i)
+// 		for j := 0; j < 10; j++ {
+// 			mutex.Lock()
+// 			go func() {
+// 				fmt.Printf("%d %d\n", i, j)
+// 				mutex.Unlock()
+
+// 			}()
+// 		}
+// 	}
+
+// }
+
+// //Not sure why it prints the value 10 even with the use of Mutex
+
+
+
 package main
 
 import (
 	"fmt"
 	"runtime"
-	"sync"
 )
 
 func init() {
@@ -12,20 +45,13 @@ func init() {
 
 func main() {
 
-	mutex := new(sync.Mutex)
-
 	for i := 0; i < 10; i++ {
-		//fmt.Println(i)
 		for j := 0; j < 10; j++ {
-			mutex.Lock()
-			go func() {
-				fmt.Printf("%d %d\n", i, j)
-				mutex.Unlock()
-
-			}()
+			go func(m, n int) {
+				fmt.Printf("%d %d\n", m, n)
+			}(i, j)
 		}
 	}
-
+	var a string
+	fmt.Scanln(&a)
 }
-
-//Not sure why it prints the value 10 even with the use of Mutex
